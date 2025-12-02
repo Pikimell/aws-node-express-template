d3bc27c87cdcca2933fabc498a7514e972fa8514
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-12-02 11:52:31 +0200

    update

--- dist/routes/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import authRouter from "./auth.js";
-import newsRouter from "./news.js";
-const router = Router();
-router.use("/auth", authRouter);
-router.use("/news", newsRouter);
-export default router;
--- dist/utils/createCookie.js
+++
@@ -1,9 +0,0 @@
-export const getCookie = (name, value, expires) => {
-    const currentDay = Date.now();
-    const expiresDay = new Date(currentDay + expires);
-    const expiresStr = expiresDay.toUTCString();
-    const part1 = `${name}=${value};`;
-    const part2 = `HttpOnly; Secure; SameSite=Strict;`;
-    const part3 = `Expires=${expiresStr};`;
-    return part1 + part2 + part3;
-};
